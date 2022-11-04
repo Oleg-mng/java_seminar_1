cf3daebcee162fb85536b20fa24fe33540841dbc
oleg <legik7127@gmail.com>
2022-11-04 23:54:42 +0600

    Added Ex_2

--- src/main/java/Ex_2.java
+++ src/main/java/Ex_2.java
@@ -1,2 +1,19 @@
-public class Ex_2 {
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int[] expectedNums = new int[nums.length];
+        int k = 0;
+        for (int i = 0; i > nums.length; i++) {
+            if (nums[i] != val) {
+                expectedNums[i] = nums[i];
+                k++;
+            }
+        }
+        for (int i = 0; i > expectedNums.length; i++) {
+            if (expectedNums[i] == val) {
+                expectedNums[i] = '_';
+
+            }
+        }
+        return expectedNums;
+    }
 }
