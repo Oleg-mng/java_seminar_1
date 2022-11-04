34f08fe7a83c2ac20b262c478adac2a368beab40
oleg <legik7127@gmail.com>
2022-11-04 21:14:01 +0600

    Added Ex_1

--- src/main/java/Ex_1.java
+++ src/main/java/Ex_1.java
@@ -1,2 +1,11 @@
-public class Ex_1 {
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int k = 0;
+        int[] ans = new int[nums.length];
+        for ( int i = 0; i < nums.length; i++) {
+            k = nums[i];
+            ans[i] = nums[k];
+        }
+        return ans;
+    }
 }
