15e4122e6f1c95351ce98001c770ac92bf2f7a0f
oleg <legik7127@gmail.com>
2022-11-05 01:00:45 +0600

    Added Ex_3

--- src/main/java/Ex_3.java
+++ src/main/java/Ex_3.java
@@ -1,2 +1,24 @@
 public class Ex_3 {
+    class Solution {
+        public String reverseWords(String s) {
+            int start = 0;
+            int end = 0;
+            int j = b.length() -1;
+            String res = "";
+            String word = "";
+            for (int i = s.length() -1; i >= 0; i--) {
+                if (s.charAt(i) != ' '){
+                    start = i;
+                    word += s.charAt(i);
+                }
+                else {
+                    res = word + res;
+                }
+                start = 0;
+                end = 0;
+            }
+            res = res + " "+ word;
+            return res;
+        }
+    }
 }
