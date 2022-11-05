ba119b631f99db0263c757bfb767ab8713833ccc
oleg <legik7127@gmail.com>
2022-11-05 12:22:28 +0600

    Changes in Ex_4

--- src/main/java/Ex_4.java
+++ src/main/java/Ex_4.java
@@ -1,5 +1,14 @@
-public class Ex_4 {
-    public static void main(String[] args) {
-        System.out.printf("First steps");
+class Solution {
+    public boolean isPalindrome(String s) {
+        int j =  s.length() - 1;
+        for (int i = 0; i < s.length()/2; i++) {
+            j -= i;
+            if (Character.toUpperCase(s.charAt(i)) != Character.toUpperCase(s.charAt(j))) {
+                return false;
+                // рассмотреть случай Заглавных и строчных букв
+            }
+        }
+        return true;
     }
 }
+
